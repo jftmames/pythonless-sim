6ae24d9fde7bc6ef786ae8c5e1441833beffa102
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-21 21:47:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="min-h-dvh bg-white text-gray-900">{children}</body>
+      <body className="min-h-dvh bg-white text-gray-900">
+        {children}
+      </body>
     </html>
   );
 }
