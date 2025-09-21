d686f6fa9c9c7f47da8120d11f4636f1adf4e36e
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-21 21:30:28 +0200

    Update builder.ts

--- lib/builder.ts
+++ lib/builder.ts
@@ -1,1 +1,12 @@
+import { Step } from "./types";
+
+let counter = 0;
+
+export function makeStep(p: Omit<Step, "index" | "id">): Step {
+  return { id: `s${++counter}`, index: counter, ...p };
+}
+
+export function resetStepCounter() {
+  counter = 0;
+}
 
